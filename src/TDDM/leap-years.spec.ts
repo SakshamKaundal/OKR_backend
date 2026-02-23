c8dae3d9948feb67933747b1c3265d89db8d7b69
Saksham Kaundal <kaundalsaksham@gmail.com>
2026-02-23 17:21:06 +0530

    Green-divisible by 4

--- src/TDDM/leap-years.spec.ts
+++ src/TDDM/leap-years.spec.ts
@@ -5,4 +5,7 @@ describe('isLeapYear', () => {
   it('should return false if year is not divisile by 4', () => {
     expect(isLeapYear(2021)).toBe(false);
   });
+  it('should return true if year is divisible by 4', () => {
+    expect(isLeapYear(2020)).toBe(true);
+  });
 });
