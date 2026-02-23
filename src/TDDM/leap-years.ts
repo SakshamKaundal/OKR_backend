2c6f93556620085c0ace83e99c94c4c325aab6f1
Saksham Kaundal <kaundalsaksham@gmail.com>
2026-02-23 17:39:58 +0530

    Refactor

--- src/TDDM/leap-years.ts
+++ src/TDDM/leap-years.ts
@@ -1,6 +1,4 @@
 export function isLeapYear(year: number): boolean {
-  if (year % 400 === 0) return true;
-  if (year % 100 === 0) return false;
-  if (year % 4 === 0) return true;
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) return true;
   return false;
 }
