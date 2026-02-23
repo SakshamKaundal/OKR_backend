0363a62b93a983b31a258b582b83b6dbba4ba2be
Saksham Kaundal <kaundalsaksham@gmail.com>
2026-02-23 17:28:45 +0530

    Green-divisible by 4,divisble by 100 but not by 400

--- src/TDDM/leap-years.spec.ts
+++ src/TDDM/leap-years.spec.ts
@@ -8,4 +8,7 @@ describe('isLeapYear', () => {
   it('should return true if year is divisible by 4', () => {
     expect(isLeapYear(2020)).toBe(true);
   });
+  it('should return false if year is divisible by 4, divisible by 100 but not by 400', () => {
+    expect(isLeapYear(2100)).toBe(false);
+  });
 });
--- src/TDDM/leap-years.ts
+++ src/TDDM/leap-years.ts
@@ -1,4 +1,5 @@
 export function isLeapYear(year: number): boolean {
-  if(year % 4 === 0) return true;
-    return false;
+  if (year % 4 === 0 && year % 100 === 0 && year % 400 !== 0) return false;
+  if (year % 4 === 0) return true;
+  return false;
 }
